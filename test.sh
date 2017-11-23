82bbfb56fd8f6f9dd708229478edb19747bea046
Andrej <Andrej@Vistadraft.com>
2017-11-23 20:41:04 +0200

    fdf

--- test.sh
+++ test.sh
@@ -1,28 +1,3 @@
-#!/bin/bash
-check_rc() {
-  # exit if passed in value is not = 0
-  # $1 = return code
-  # $2 = command / label
-  if [ $1 -ne 0 ]
-  then
-    echo "$2 command failed"
-    exit 1
-  fi
-}
-
-# fiansding files that differ from this commit and master
-check_rc $? 'echo git fetch'
-git fetch
-check_rc $? 'git fetch'
-check_rc $? 'echo git diff'
-diff_files=`git diff --name-only origin/master | xargs`
-check_rc $? 'git diff'
-for x in ${diff_files}
-do
-   if [ "$x" == "a.txt" ]; then
-        echo "a.txt was changed"
-	    exit	
-	else echo "no changes" 
-   fi
-done
- 
+git add .
+git commit -m "fdf"
+git push
